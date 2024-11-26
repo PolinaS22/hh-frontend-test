921c8268825007c35f1719e86c6924635e53a163
PolinaS22 <shishkinapln@yandex.ru>
2024-11-26 13:36:13 +0300

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -49,7 +49,7 @@ const handleSubmit = async (e) => {
   setIsSubmitting(true);
 
 try {
-    const response = await fetch("http://localhost:8080/proxy", {
+    const response = await fetch("https://hh-backend-test-1.onrender.com/proxy", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
